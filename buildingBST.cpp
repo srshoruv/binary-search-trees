2916e9ede1712cb426e877d68fd1611c4860f759
srshoruv <srshoruv2002@gmail.com>
2024-09-21 04:08:49 +0600

    delete a node problem solved

--- buildingBST.cpp
+++ buildingBST.cpp
@@ -64,6 +64,42 @@ bool search(Node* root, int key) {
     }
 }
 
+Node* getIS(Node* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* deleteNode(Node* root, int val) {
+    if (root==NULL) {
+        return NULL;
+    }
+
+    if ( val < root->data) {
+        root->left = deleteNode(root->left, val);
+    } else if (val > root->data) {
+        root->right = deleteNode(root->right, val);
+    } else {
+        // case 1: 0 child
+        if (root->left == NULL && root->right == NULL ) {
+            delete root;
+            return NULL;
+
+            //case 2: 1 child
+        } 
+       else if (root->left == NULL || root->right == NULL) {
+            return root->left == NULL ? root->right : root->left;
+        } else {
+            Node* IS = getIS(root->right);
+            root->data = IS->data;
+            root->right = deleteNode(root->right, IS->data);
+        } 
+            
+    
+    }
+    return root;
+}
 
 
 int main() {
@@ -71,8 +107,9 @@ int main() {
     int n = vec.size()-1;
 
     Node* root = buildBST(vec,n);
-    // inorder(root);
-
-    cout << search(root, 7);
+    inorder(root);
+    cout << endl;
+    deleteNode(root, 7);
+    inorder(root);
 
 }
